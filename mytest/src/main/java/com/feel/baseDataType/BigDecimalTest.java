8cc0b910e8ef6686a4a011d5adeba728b7e73f91
liyl <917326652@qq.com>
2017-10-17 15:58:35 +0800

    test case

---
+++ mytest/src/main/java/com/feel/baseDataType/BigDecimalTest.java
@@ -0,0 +1,12 @@
+package com.feel.baseDataType;
+
+import java.math.BigDecimal;
+
+public class BigDecimalTest {
+    private static BigDecimal b = new BigDecimal("-0.9");
+
+    public static void main(String[] args) {
+        // 未初始化使用会报错
+        System.out.println(b.intValue());
+    }
+}
